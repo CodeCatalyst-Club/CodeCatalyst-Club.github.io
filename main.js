f5cd428ac06cd6d612e0d736bd68e833578918ce
Code Catalyst <128047452+CodeCatalyst-Club@users.noreply.github.com>
2023-03-16 18:38:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const ins = document.queryselector(".fa-brands fa-instagram");
+const github = document.queryselector(".fa-brands fa-github");
+const whats = document.queryselector(".fa-brands fa-whatsapp");
+
+function tick_1(){
+    window.open("https://www.instagram.com/code_catalystt/");
+}
+
+function tick_2(){
+    window.open("https://github.com/CodeCatalyst-Club")
+}
+
+function tick_3(){
+    window.open("https://chat.whatsapp.com/CNCVHECYZsGGbPycaLVNd9");
+}
