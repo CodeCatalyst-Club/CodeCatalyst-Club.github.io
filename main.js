7392f842e84c77206d56bbda37ce5ef6db4b915f
Code Catalyst <128047452+CodeCatalyst-Club@users.noreply.github.com>
2023-03-24 18:24:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,90 @@
 const ins = document.queryselector(".fa-brands fa-instagram");
 const github = document.queryselector(".fa-brands fa-github");
-const whats = document.queryselector(".fa-brands fa-whatsapp");
 
-function tick_1(){
-    window.open("https://www.instagram.com/code_catalystt/");
+
+function tick_1() {
+  window.open("https://www.instagram.com/code_catalystt/");
 }
 
-function tick_2(){
-    window.open("https://github.com/CodeCatalyst-Club")
+function tick_2() {
+  window.open("https://github.com/CodeCatalyst-Club");
 }
-function tick_4(){
-    window.open("https://escai.rf.gd/");
+
+
+function tick_4() {
+  window.open("https://escai.rf.gd/");
 }
-function tick_5(){
-    window.open("https://gtprojectz.blogspot.com");
+
+function tick_5() {
+  window.open("https://gtprojectz.blogspot.com/");
 }
+
+
+
+
+
+function swap() {
+  let hamburger_section = document.querySelector(".hamburger_section");
+  let z = document.getElementById("hidden_section");
+  hamburger_section.classList.toggle("change");
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "block";
+  }
+}
+
+function scrollWin1() {
+  window.scrollBy(0, 0, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+
+function scrollWin2() {
+  window.scrollBy(100, 1300, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scrollWin3() {
+  window.scrollBy(100, 2200, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scrollWin4() {
+  window.scrollBy(100, 3700, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scrollWin5() {
+  window.scrollBy(100, 4700, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+
+
+function scroll1() {
+  window.scrollBy(0, 0, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+
+function scroll2() {
+  window.scrollBy(100, 1230, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scroll3() {
+  window.scrollBy(100, 1250, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scroll4() {
+  window.scrollBy(100, 4100, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
+function scroll5() {
+  window.scrollBy(100, 6000, "pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
+  
+}
+
