07f7f4eb0cdc4260b3cbfffb93bf7acec9d85c3b
Code Catalyst <128047452+CodeCatalyst-Club@users.noreply.github.com>
2023-03-26 18:20:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -71,3 +71,30 @@ function scroll4() {
 function scroll5() {
   window.open("https://goo.gl/maps/XMnnFeZjZY5m553g9");
 }
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function read_more() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
